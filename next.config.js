19d634034c08f01597a81570029df14eb9915d22
Guy Sopher <guysopher@github.com>
2025-03-16 21:21:44 +0200

    Refactor webpack configuration in next.config.js: mark esbuild as external for server builds

--- next.config.js
+++ next.config.js
@@ -1,39 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  // your other Next.js config options here
-};
-
-// Only run the custom HTTPS server in development mode
-if (process.env.NODE_ENV !== 'production') {
-  const { createServer } = require('https');
-  const { parse } = require('url');
-  const next = require('next');
-  const fs = require('fs');
-  const path = require('path');
-
-  const dev = true;
-  const app = next({ dev });
-  const handle = app.getRequestHandler();
-
-  try {
-    const httpsOptions = {
-      key: fs.readFileSync(path.join(__dirname, 'certificates/localhost-key.pem')),
-      cert: fs.readFileSync(path.join(__dirname, 'certificates/localhost.pem')),
-    };
-
-    app.prepare().then(() => {
-      createServer(httpsOptions, (req, res) => {
-        const parsedUrl = parse(req.url, true);
-        handle(req, res, parsedUrl);
-      }).listen(3001, (err) => {
-        if (err) throw err;
-        console.log('> Ready on https://localhost:3001');
-      });
-    });
-  } catch (error) {
-    console.error('Failed to start HTTPS server:', error);
-  }
+  webpack: (config, { isServer }) => {
+    // Mark esbuild as external to prevent bundling
+    if (isServer) {
+      config.externals = [...(config.externals || []), 'esbuild']
+    }
+    
+    return config
+  },
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
